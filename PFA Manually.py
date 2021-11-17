333a8fc8d3e39f6021bcd2d2d355bfe2b82e5e9e
Nora <nora.pauelsen@gmail.com>
2021-11-17 20:55:52 +0100

    changed PFA Manually

--- PFA Manually.py
+++ PFA Manually.py
@@ -3,7 +3,7 @@
 # Code found in: https://github.com/theophilee/learner-performance-prediction, train_lr.py
 
 # TODO: Question -> How do I import functions from other script? This gives an error
-# from PerformanceFactorAnalysis_BayesianKnowledgeTracing.py import compute_metrics
+from PerformanceFactorAnalysis_BayesianKnowledgeTracing import compute_metrics
 
 import argparse
 import numpy as np
@@ -13,31 +13,73 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import roc_auc_score, accuracy_score, log_loss, brier_score_loss
 
 
-def compute_metrics(y_pred, y):
-    acc = accuracy_score(y, np.round(y_pred))
-    auc = roc_auc_score(y, y_pred)
-    nll = log_loss(y, y_pred)
-    mse = brier_score_loss(y, y_pred)
-    return acc, auc, nll, mse
-
 # Load dataset of bridge_algebra06
 train_df = pd.read_csv(r"data/bridge_algebra06/preprocessed_data_train.csv", sep="\t")
 test_df = pd.read_csv(r"data/bridge_algebra06/preprocessed_data_test.csv", sep="\t")
-full_data = pd.read_csv(r"data/bridge_algebra06/preprocessed_data.csv", sep="\t")
-print(test_df)
+#full_data = pd.read_csv(r"data/bridge_algebra06/preprocessed_data.csv", sep="\t")
+print(train_df)
 
-#Tryout for 1 student:?
+#Tryout for 1 student (student_id = 0) and 1 skill (191)
 # #Question: Or how to group students together?
-# train_student = train_df[(train_df["user_id"] == "0")]
+df_student = train_df[train_df["user_id"] == 0]
+df_student.describe(include="all")
+f = df_student.groupby('skill_id').agg({'timestamp': 'count', 'correct': 'count'})
+f.timestamp.max() #skill_id 191 has most practices (74)
+df_student_skill = df_student[df_student["skill_id"] == 191]
+df_student_skill = df_student_skill.drop(columns = ["user_id", "skill_id"])
+df_student_skill.groupby("timestamp"). agg({"item_id": "count", "correct": "count"})
 
-#Formula:
-#Question: Do I need S and K individually or is "correct" covering both as a binary variable? And is beta the intercept included automatically?
+#Make column for time
+df_student_skill = df_student_skill.reset_index()
+sort = df_student_skill.sort_values(by = "timestamp")
+print(sort)
+df_student_skill["time"] = range(1, len(df_student_skill)+1)
+
+
+#Calculate S sequentially (prior successes) and F (prior failures)
+df_student_skill["prior_successes"] = 0
+df_student_skill["prior_failures"] = 0
+
+"""
+for index, row in df_student_skill.iterrows():
+    print("number of iteration is", index)
+    if row["correct"] == 1:
+        row["prior_successes"] = row["prior_successes"] + 1
+        print(row["prior_successes"][index-1])
+
+"""
+for index in df_student_skill.index:
+    print("number of iteration is", index)
+    if df_student_skill.loc[index,'correct'] == 1:
+        df_student_skill.loc[index, 'prior_successes'] = df_student_skill.iloc[0:index]["correct"].sum() + 1
+
+    elif df_student_skill.loc[index, "correct"] == 0:
+        df_student_skill.loc[index, "prior_failures"] = df_student_skill.iloc[0:index]["prior_failures"].sum() + 1
+
+#TODO: Question: How to sum over all rows where correct = 0 and indexing [0:index]???
+print(df_student_skill.iloc[0:3].sum())
+
+
+# print(df_student_skill.loc[df_student_skill["correct"].isin(["1"]), "correct"].sum())
+# df_student_skill.iloc[0:index]["prior_failures"].sum()
+# print(df_student_skill.loc[df_student_skill["correct"] == "0"].sum())
+
+
+
+for index in df_student_skill.index:
+    print("number of iteration is", index)
+    if df_student_skill.loc[index,'correct'] == 1:
+        df_student_skill.loc[index, 'prior_successes'] = df_student_skill.loc[index, "prior_successes"] + 1
+
+    elif df_student_skill.loc[index, "correct"] == 0:
+        df_student_skill.loc[index, "prior_failures"] = df_student_skill.loc[index, "prior_failures"] + 1
+
+
+
 #S_k = number of correctly solved tasks
-S_train = train_df[(train_df["correct"] == "1")]
-S_test = test_df[(train_df["correct"] == "1")]
+#S_train = train_df[(train_df["correct"] == "1")]
 #F_sk = number of incorrectly solved tasks
-F_train = train_df[(train_df["correct"] == "0")]
-F_test = test_df[(train_df["correct"] == "0")]
+#F_train = train_df[(train_df["correct"] == "0")]
 
 #m_train =
 
